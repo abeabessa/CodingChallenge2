a4d9b2ad707a989a0e17e89c519465b462b6c524
abeabessa <bbessamarinho@gmail.com>
2024-09-08 23:05:51 -0400

    Create and test calculate Tip function

--- cc_2.js
+++ cc_2.js
@@ -4,3 +4,20 @@ cc_2.js
 
 const bill_size = 150
 const tip = (bill_size >= 50 && bill_size <= 300) ? bill_size * 0.15 : bill_size * 0.20;
+
+// Output Details
+
+console.log(`The bill was $${bill_size} , the tip was $${tip}, and the total value is $${bill_size + tip}`); 
+
+// Create a Function
+
+function calculateTip (bill_size) {
+    return (bill_size>= 50 && bill_size<= 300) ? bill_size * 0.15 : bill_size * 0.20;
+}
+
+// Using bill value $100
+
+const testBill = 100;
+const testTip = calculateTip(testBill);
+
+console.log(` Considering the bill $${testBill}, the tip is $${ testTip}, and the total value is $${(testBill + testTip)} `);
